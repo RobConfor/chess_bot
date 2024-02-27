9d4343823c51ea413afeab2580eb7e082ee1db09
Robert <rconforti11@gmail.com>
2024-02-26 20:32:27 -0700

    inital

--- Board.cpp
+++ Board.cpp
@@ -6,13 +6,17 @@ Board::Board(){
 
 }
 
-    //can make a particular initilizer that can set up chess puzzles
-    //takes in peice placesment as an array
+
+//a particular initilizer that can set up chess puzzles
+//takes in peice placesment as an array in chess notation
+Board::Board(std::string piece[]){
+
+}
     
 
-    //REQUIRES: move is given in chess notation
-    //REQUIRES: peice moves in its correct pathing
-    //also takes peice if required
+//REQUIRES: move is given in chess notation
+//REQUIRES: peice moves in its correct pathing
+//also takes peice if required
 void Board::move_peice(std::string move){
 
 }
