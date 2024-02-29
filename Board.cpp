f7e6789dca0bf8a3590495299e28d463dfbe123c
Robert <rconforti11@gmail.com>
2024-02-29 09:02:20 -0700

    notaion function

--- Board.cpp
+++ Board.cpp
@@ -33,12 +33,33 @@ void Board::move_peice(std::string move){
     //  Kxe3 king takes pawn (we have to dedce which king, but we can if we know if its white or black turn)
     //  Qd5+ queen checks king
 int notation_converter(std::string pos, bool &takes, int &value){
+
+    //is taking a piece
+    if(pos.find('x') != std::string::npos){
+        takes = true;
+    } else {
+        takes = false;
+    }
+
     
-    //removes all letter and only leaves number which is final position
-    int end_pos = stoi(pos);
+    if(islower(pos[0])){
+        //we know it is a pawn move
+        value = 1;
+
+        //nested yes ik yuck
+        if(takes == false){
+
+            //this is not correct
+            //stoi removes all letters and leaves just numbers
+            return stoi(pos);
+        }
+
+        return ;
+
+    }
 
 
-    return ; 
+    return ;
 }
 
 //I j copy pasted ts idk why it got so many const declarations like bro we get it we cant change u
@@ -47,5 +68,4 @@ constexpr const char *const PIECE_NAMES[] = {
   "B", // Bishop
   "R",  // Rook
   "Q",  // Queen
-  "K",   // King
 };
